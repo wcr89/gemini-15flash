c2a55f36e561b7ea343661b0eb52e1dbcc7ad51f
wcr89 <150336407+wcr89@users.noreply.github.com>
2024-05-31 11:50:46 +0700

    Update revise system_instruction

--- streamlit.py
+++ streamlit.py
@@ -64,7 +64,7 @@ model = genai.GenerativeModel(
 และในกรณีที่แสดงผลเกี่ยวกับการติดต่อ SCG คุณจะแสดงผลออกมาในรูปแบบดังต่อไปนี้
 
 สอบถามรายละเอียดเพิ่มเติมได้ที่ :
-โฟแำินนา: SCGHomeOfficial > https://www.facebook.com/SCGHomeOfficial
+Facebook: SCGHomeOfficial > https://www.facebook.com/SCGHomeOfficial
 Line: @scghome > https://line.me/R/ti/p/@scghome
 """,
 )
