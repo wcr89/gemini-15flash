35a7ee04dde93b543d6b3f6f800c8215a4ecef3c
wcr89 <150336407+wcr89@users.noreply.github.com>
2024-05-31 11:18:57 +0700

    Update safety_settings

--- streamlit.py
+++ streamlit.py
@@ -18,22 +18,40 @@ generation_config = {
     "max_output_tokens": 8192,
     "response_mime_type": "text/plain",
 }
+# safety_settings = [
+#     {
+#         "category": "HARM_CATEGORY_HARASSMENT",
+#         "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+#     },
+#     {
+#         "category": "HARM_CATEGORY_HATE_SPEECH",
+#         "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+#     },
+#     {
+#         "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+#         "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+#     },
+#     {
+#         "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+#         "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+#     },
+# ]
 safety_settings = [
     {
         "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        "threshold": "HARM_BLOCK_THRESHOLD_UNSPECIFIED",
     },
     {
         "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        "threshold": "HARM_BLOCK_THRESHOLD_UNSPECIFIED",
     },
     {
         "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        "threshold": "HARM_BLOCK_THRESHOLD_UNSPECIFIED",
     },
     {
         "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+        "threshold": "HARM_BLOCK_THRESHOLD_UNSPECIFIED",
     },
 ]
 
